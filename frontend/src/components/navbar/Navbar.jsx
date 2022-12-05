fc764d6ff95e59f757e867e83b5de5a28516ac7e
swetakarar <kararsweta@gmail.com>
2022-12-05 21:15:15 +0530

    fix: navbar with 2 issues

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -2,47 +2,42 @@ import React from "react";
 import "./navbar.css";
 import { Routes, Route, Link, BrowserRouter as Router } from "react-router-dom";
 import logo1 from "../../assets/elipse.svg";
-import profile from "../../assets/profile.svg"
+import search from "../../assets/search.svg";
+import profile from "../../assets/profile.svg";
 
 const Navbar = () => {
   return (
     <>
       <nav role="navigation">
         <ul id="menu">
-          <Link to="/" style={{ textDecoration: "none" }}>
-            <img src={logo1} alt="logo" className="logo"/>
-          </Link>
-          <Link to="./projects" style={{ textDecoration: "none" }}>
-            <p>Projects</p>
-          </Link>
-          <Link to="./ranks" style={{ textDecoration: "none" }}>
-            <p>Ranks</p>
-          </Link>
-          <Link to="./blogs" style={{ textDecoration: "none" }}>
-            <p>Blogs</p>
-          </Link>
-          <Link to="/" style={{ textDecoration: "none" }}>
-            <img src={profile} alt="logo" className="profile"/>
-          </Link>
+          <div className="left">
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <img src={logo1} alt="logo" className="logo" />
+            </Link>
+            <Link to="./projects" style={{ textDecoration: "none" }}>
+              <p>Projects</p>
+            </Link>
+            <Link to="./ranks" style={{ textDecoration: "none" }}>
+              <p>Ranks</p>
+            </Link>
+            <Link to="./blogs" style={{ textDecoration: "none" }}>
+              <p>Blogs</p>
+            </Link>
+          </div>
+          <div className="right">
+            <div className="input">
+              <img src={search} alt="searchicon" id="searchId" />
+              <input type="search" placeholder="Search..." id="searchInput" />
+            </div>
+
+            <div className="upload">
+              <button>Upload</button>
+            </div>
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <img src={profile} alt="logo" className="profile" />
+            </Link>
+          </div>
         </ul>
-
-        {/* <div className="Logo"></div>
-
-        <div className="Projects">Projects</div>
-
-        <div className="Ranking">Ranking</div>
-
-        <div className="Blogs">Blogs</div>
-
-        <div className="Search-button">
-          <button type="search"> </button>
-        </div>
-
-        <div className="Upload-button">
-          <button type="upload"> </button>
-        </div>
-
-        <div className="profile"></div> */}
       </nav>
     </>
   );
