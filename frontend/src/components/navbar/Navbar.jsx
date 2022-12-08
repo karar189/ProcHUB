4bb2da714681e2c369a5dbd171d7a354a031f0e8
swetakarar <kararsweta@gmail.com>
2022-12-08 10:42:40 +0530

    add: mui

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 import { Routes, Route, Link, BrowserRouter as Router } from "react-router-dom";
 import logo1 from "../../assets/elipse.svg";
 import search from "../../assets/search.svg";
 import profile from "../../assets/profile.svg";
 
+// import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
 const Navbar = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <>
       <nav role="navigation">
@@ -31,11 +48,47 @@ const Navbar = () => {
             </div>
 
             <div className="upload">
-              <button>Upload</button>
+              <a href="">Upload</a>
             </div>
             <Link to="/" style={{ textDecoration: "none" }}>
               <img src={profile} alt="logo" className="profile" />
             </Link>
+            <div>
+              <Button variant="outlined" onClick={handleClickOpen}>
+                SignIn
+              </Button>
+              <Dialog open={open} onClose={handleClose}>
+                {/* <DialogTitle>Subscribe</DialogTitle> */}
+                <DialogContent>
+                  {/* <DialogContentText>
+                    To subscribe to this website, please enter your email
+                    address here. We will send updates occasionally.
+                  </DialogContentText> */}
+                  <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="Email Address"
+                    type="email"
+                    fullWidth
+                    variant="standard"
+                  />
+                  <TextField
+                    autoFocus
+                    margin="dense"
+                    id="password"
+                    label="Password"
+                    type="email"
+                    fullWidth
+                    variant="standard"
+                  />
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose}>Sign In</Button>
+                  <Button onClick={handleClose}>Create Account</Button>
+                </DialogActions>
+              </Dialog>
+            </div>
           </div>
         </ul>
       </nav>
