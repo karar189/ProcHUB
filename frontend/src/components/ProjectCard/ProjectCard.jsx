c907b5ed7352a4fc8cb7455f9a2ff6d2b1f7f6f4
swetakarar <kararsweta@gmail.com>
2022-12-14 17:22:32 +0530

    add: logic for project image

--- frontend/src/components/ProjectCard/ProjectCard.jsx
+++ frontend/src/components/ProjectCard/ProjectCard.jsx
@@ -7,14 +7,17 @@ import linkedn from '../../assets/linkedn.svg';
 
 import proc1 from '../../assets/img-proc.png';
 
-
 const ProjectCard = ({ project }) => {
-console.log(project);
+  console.log(project);
   return (
     <>
       <div className="box-card">
         <div className="img-proc">
-          <img src={proc1} alt="background-img" />
+          {project.image ? (
+            <img src={project.image} alt="background-img" />
+          ) : (
+            <img src={proc1} alt="background-img" />
+          )}
         </div>
         <div className="content">
           <h1>{project.title}</h1>
