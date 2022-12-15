9b6ed66020a0a6cdb955a37c81b065425f4be912
swetakarar <kararsweta@gmail.com>
2022-12-15 16:01:49 +0530

    style: filter in catalouge page

--- frontend/src/pages/catalogue/Catalogue.jsx
+++ frontend/src/pages/catalogue/Catalogue.jsx
@@ -26,40 +26,76 @@ const Catalogue = () => {
         <Container>
           <div className="inner-box">
             <div className="left-filter">
-              <Accordion sx={{ background: 'transparent', color: '#ffff' }}>
+              <Accordion
+                sx={{ background: 'transparent', color: '#ffff' }}
+                //expandicon
+              >
                 <AccordionSummary aria-controls="panel1a-content" id="panel1a-header">
-                  <Typography>Filter 1</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Typography>Lorem ipsum dolor</Typography>
-                </AccordionDetails>
-              </Accordion>
-              <br />
-              <Accordion sx={{ background: 'transparent', color: '#ffff' }}>
-                <AccordionSummary aria-controls="panel1a-content" id="panel1a-header">
-                  <Typography>Filter 2</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Typography>Lorem ipsum dolor</Typography>
-                </AccordionDetails>
-              </Accordion>
-              <br />
-              <Accordion sx={{ background: 'transparent', color: '#ffff' }}>
-                <AccordionSummary aria-controls="panel1a-content" id="panel1a-header">
-                  <Typography>Filter 3</Typography>
+                  <Typography>All</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
                   <Typography>Lorem ipsum dolor</Typography>
                 </AccordionDetails>
               </Accordion>
 
-              {/* <script src="./logic.js"></script> */}
+              <Accordion sx={{ background: 'transparent', color: '#ffff' }}>
+                <AccordionSummary aria-controls="panel1a-content" id="panel1a-header">
+                  <Typography>Infrastrusture</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>Project name 1#</Typography>
+                </AccordionDetails>
+                <AccordionDetails>
+                  <Typography>Project name 1#</Typography>
+                </AccordionDetails>
+                <AccordionDetails>
+                  <Typography>Project name 1#</Typography>
+                </AccordionDetails>
+                <AccordionDetails>
+                  <Typography>Project name 1#</Typography>
+                </AccordionDetails>
+              </Accordion>
+
+              <Accordion sx={{ background: 'transparent', color: '#ffff' }}>
+                <AccordionSummary aria-controls="panel1a-content" id="panel1a-header">
+                  <Typography>DAPPs</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>Lorem ipsum dolor</Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion sx={{ background: 'transparent', color: '#ffff' }}>
+                <AccordionSummary aria-controls="panel1a-content" id="panel1a-header">
+                  <Typography>NFT</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>Lorem ipsum dolor</Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion sx={{ background: 'transparent', color: '#ffff' }}>
+                <AccordionSummary aria-controls="panel1a-content" id="panel1a-header">
+                  <Typography>Gaming</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>Lorem ipsum dolor</Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion sx={{ background: 'transparent', color: '#ffff' }}>
+                <AccordionSummary aria-controls="panel1a-content" id="panel1a-header">
+                  <Typography>Ecosystem</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>Lorem ipsum dolor</Typography>
+                </AccordionDetails>
+              </Accordion>
             </div>
             <div className="right-content">
-              {project &&
-                project.map((project, index) => {
-                  return <ProjectCard project={project} />;
-                })}
+              <div className="inner-right">
+                {project &&
+                  project.map((project, index) => {
+                    return <ProjectCard project={project} />;
+                  })}
+              </div>
             </div>
           </div>
         </Container>
